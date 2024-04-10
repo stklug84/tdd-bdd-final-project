0c4f41fc80d6335f5bd361942fc60b72d3892be3
Steffen Klug <45033201+stklug84@users.noreply.github.com>
2024-04-09 23:15:40 -0400

    Add REST API endpoint GET method and test read product.

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -134,15 +134,15 @@ class TestProductRoutes(TestCase):
         # Uncomment this code once READ is implemented
         #
 
-        # # Check that the location header was correct
-        # response = self.client.get(location)
-        # self.assertEqual(response.status_code, status.HTTP_200_OK)
-        # new_product = response.get_json()
-        # self.assertEqual(new_product["name"], test_product.name)
-        # self.assertEqual(new_product["description"], test_product.description)
-        # self.assertEqual(Decimal(new_product["price"]), test_product.price)
-        # self.assertEqual(new_product["available"], test_product.available)
-        # self.assertEqual(new_product["category"], test_product.category.name)
+        # Check that the location header was correct
+        response = self.client.get(location)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        new_product = response.get_json()
+        self.assertEqual(new_product["name"], test_product.name)
+        self.assertEqual(new_product["description"], test_product.description)
+        self.assertEqual(Decimal(new_product["price"]), test_product.price)
+        self.assertEqual(new_product["available"], test_product.available)
+        self.assertEqual(new_product["category"], test_product.category.name)
 
     def test_create_product_with_no_name(self):
         """It should not Create a Product without a name"""
@@ -163,10 +163,50 @@ class TestProductRoutes(TestCase):
         response = self.client.post(BASE_URL, data={}, content_type="plain/text")
         self.assertEqual(response.status_code, status.HTTP_415_UNSUPPORTED_MEDIA_TYPE)
 
-    #
-    # ADD YOUR TEST CASES HERE
-    #
+    def test_get_product(self):
+        """Get a product"""
+        # Create one product and save it
+        test_product = self._create_products(1)[0]
+        # Call GET on API
+        response = self.client.get(f"{BASE_URL}/{test_product.id}")
+        # Check return code
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        # Check response data
+        data = response.get_json()
+        self.assertEqual(data["name"], test_product.name)
 
+    def test_get_product_not_found(self):
+        """Do not retrive a non-existing product"""
+        # Ask for product 0 that does not exist
+        response = self.client.get(f"{BASE_URL}/0")
+        # Check if 404 is returned
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+        # Return data should contain error message
+        data = response.get_json()
+        self.assertIn("was not found!", data["message"])
+
+    ######################################################################
+    # READ A PRODUCT
+    ######################################################################
+    @app.route("/products/<int:product_id>", methods=["GET"])
+    def get_products(product_id):
+        """
+        Retrieve a single Product
+
+        This endpoint will return a Product based on it's id
+        """
+        app.logger.info("Request to Retrieve a product with id [%s]", product_id)
+
+        # Find product
+        product = Product.find(product_id)
+        
+        # Abort with 404
+        if not product:
+            abort(status.HTTP_404_NOT_FOUND, f"Product '{product_id}' was not found!")
+
+        # Return serialized product
+        return product.serialize(), status.HTTP_200_OK
+    
     ######################################################################
     # Utility functions
     ######################################################################
