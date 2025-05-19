fadffe2dae5040cab00aa72d4f60d5884e96aa43
Steffen Klug <45033201+stklug84@users.noreply.github.com>
2025-05-19 08:44:29 +0000

    feat: add FuzzyChoice for available field

--- tests/factories.py
+++ tests/factories.py
@@ -34,4 +34,5 @@ class ProductFactory(factory.Factory):
     name = FuzzyChoice(["Hat", "Pants", "Shirt", "Apple", "Banana", "Pots", "Towels", "Ford", "Chevy", "Hammer", "Wrench"])
     description = factory.Faker("text")
     price = FuzzyDecimal(0.5, 2000.0, 2)
+    available = FuzzyChoice([True, False])
    ## Add code to create Fake Products 
